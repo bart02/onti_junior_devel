4d4f99b23710d5cb2e89c568dcb8f5c9c3485aef
Artem Batalov <a@batalov.me>
2020-11-21 23:03:28 +0300

    rangefinder

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ git clone https://github.com/bart02/onti_junior_devel.git
 cd onti_junior_devel
 
 cp -rf model/* /home/clover/.gazebo/models/
+cp -f simulator.launch /home/clover/catkin_ws/src/clover/clover_simulation/launch/
 cp -f parquet.jpg /home/clover/catkin_ws/src/clover/clover_simulation/models/parquet_plane/materials/textures/
 cp -f clover.world /home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/
 cp -f onti.py /home/clover/catkin_ws/src/clover/clover_simulation/scripts/
